cc9c401e5958c4a5ad25c0ee46b9a044e1c91866
Anna Lutsenko <anna@searis.com>
2025-10-04 11:54:07 +0200

    add project files

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Action Demo
+//
+//  Created by Anna Lutsenko on 04/10/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
